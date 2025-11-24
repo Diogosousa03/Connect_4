16bbac76006c28089608e229d32335a85a7c5cfa
Diogosousa03 <dmrllsousa@gmail.com>
2025-11-24 01:14:27 +0000

    board representation almost done

--- 1ºtrabalho/Board/board.pl
+++ 1ºtrabalho/Board/board.pl
@@ -2,20 +2,38 @@
 
 %Board representation and printing
 
-:- module(board, [empty_board/1]).
+:- module(board, [empty_board/1,print_board/1]).
 
 
 empty_board([
-    [empty, empty, empty, empty, empty, empty, empty],
-    [empty, empty, empty, empty, empty, empty, empty],
-    [empty, empty, empty, empty, empty, empty, empty],
-    [empty, empty, empty, empty, empty, empty, empty],
-    [empty, empty, empty, empty, empty, empty, empty],
-    [empty, empty, empty, empty, empty, empty, empty]
+    [' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ']
 ]).
 print_board(Board) :-
     nl,
-    write(' 1  2  3  4  5  6  7'), nl,
+    write(' 1 2 3 4 5 6 7'), nl,
     write('---------------------'), nl,
     print_rows(Board),
     write('---------------------'), nl.
+
+% Percorre todas as linhas
+print_rows([]) :- !.
+print_rows([H|T]) :-
+    print_row(H),
+    print_rows(T).
+
+% Imprime uma linha
+print_row(H) :-
+    print_cells(H),
+    nl.
+
+% Percorre todas as células de uma linha
+print_cells([]) :- !.
+print_cells([H|T]) :-
+    write(H), write('|'),write('_'),
+    print_cells(T).    
+
--- 1ºtrabalho/main.pl
+++ 1ºtrabalho/main.pl
@@ -1,14 +1,19 @@
 
 
+:- module(main, [main/0, start/0]).
 
+%modules
+:- use_module('Board/board').
 
+main :- start.
 
 start :-
     nl,
     write('========================='), nl,
     write('   Connect 4 (Prolog)   '), nl,
     write('========================='), nl,
-    main_menu.
+    game_init.
 
-
-:- module(main, [main/0, start/0]).
+game_init:-
+    empty_board(Board),
+    print_board(Board).
